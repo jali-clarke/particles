4e93ae82e3bbecc870827b1a675f433de81379a5
jali-clarke <jinnah.ali-clarke@outlook.com>
2021-04-18 13:04:17 -0400

    simplify Eq implementations

--- src/Particle.hs
+++ src/Particle.hs
@@ -6,8 +6,14 @@ module Particle (
     newParticle
 ) where
 
-data Species = Species {_speciesId :: Int, _mass :: Double, _radius :: Double} deriving (Eq, Show)
-data Particle = Particle {_particleId :: Int, _species :: Species, _x :: Double, _y :: Double, _z :: Double} deriving (Eq, Show)
+data Species = Species {_speciesId :: Int, _mass :: Double, _radius :: Double} deriving Show
+data Particle = Particle {_particleId :: Int, _species :: Species, _x :: Double, _y :: Double, _z :: Double} deriving Show
+
+instance Eq Species where
+    species0 == species1 = _speciesId species0 == _speciesId species1
+
+instance Eq Particle where
+    particle0 == particle1 = _particleId particle0 == _particleId particle1
 
 newSpecies :: Double -> Double -> Species
 newSpecies = Species 0
