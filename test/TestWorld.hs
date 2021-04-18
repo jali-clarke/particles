c825dacafa75fa071b0447ae7f9fd9cdf653cdde
jali-clarke <jinnah.ali-clarke@outlook.com>
2021-04-18 12:35:33 -0400

    add radius to species

--- test/TestWorld.hs
+++ test/TestWorld.hs
@@ -12,6 +12,6 @@ testWorld = describe "world actions" $ do
     describe "get nearby particles" $ do
         it "should return an empty list if there are no particles in the world" $
             let world = newWorld 10 10 10
-                species = newSpecies 0.5 
+                species = newSpecies 0.5 0.75
                 particle = newParticle species 5 5 5
             in getNearbyParticles world particle `shouldBe` []
