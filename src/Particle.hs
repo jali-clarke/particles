8c1b0210a00887c5a18f10291bb02151d1c2b818
jali-clarke <jinnah.ali-clarke@outlook.com>
2021-04-18 12:37:37 -0400

    add particle charge

--- src/Particle.hs
+++ src/Particle.hs
@@ -6,10 +6,10 @@ module Particle (
     newParticle
 ) where
 
-data Species = Species {_mass :: Double, _radius :: Double} deriving (Eq, Show)
+data Species = Species {_mass :: Double, _radius :: Double, _charge :: Double} deriving (Eq, Show)
 data Particle = Particle {_species :: Species, _x :: Double, _y :: Double, _z :: Double} deriving (Eq, Show)
 
-newSpecies :: Double -> Double -> Species
+newSpecies :: Double -> Double -> Double -> Species
 newSpecies = Species
 
 newParticle :: Species -> Double -> Double -> Double -> Particle
